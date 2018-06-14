e86074562fbcf2b0f3dfbcfc4b90aa2d3d7e1cce
SINAEEE <kimshin1227@naver.com>
2018-06-14 18:25:47 +0900

    commit 180612

---
+++ json_ex.py
@@ -0,0 +1,27 @@
+
+import json
+
+#json 인코딩 : python타입문자열 -> json
+
+customer = {
+    'id':'910103',
+    'name':'김신애',
+    'history':[
+    {'date' : '2016-01-1','item':'iphone'},
+    {'date' : '2017-01-01','item':'monitor'}]
+}
+
+print(customer)
+
+print()
+jsonString = json.dumps(customer,indent=4,ensure_ascii=False)
+print(type(jsonString))
+print(jsonString)
+
+
+#JSON 디코딩 : json -> python타입 문자열
+print()
+d = json.loads(jsonString)
+print(type(d))
+print(d)
+
